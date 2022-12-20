c5b48761e98e399558d9c97d84ac5ff68dfe628a
Deiby Montoya <deiby.sk@hotmail.com>
2022-12-20 10:26:41 -0500

    New feature: Export to Excel

--- Client/Helpers/IJSExtensions.cs
+++ Client/Helpers/IJSExtensions.cs
@@ -11,4 +11,24 @@ public static class IJSExtensions
             fileName,
             Convert.ToBase64String(file)).AsTask();
     }
+
+    public static Task ShowAlert(this IJSRuntime js, string message)
+    {
+        return js.InvokeAsync<object>("Swal.fire", message).AsTask();
+    }
+    
+    public static Task ShowAlert(this IJSRuntime js, string message, string title, TypeMessageSweetAlert type)
+    {
+        return js.InvokeAsync<object>("Swal.fire", title, message, type.ToString()).AsTask();
+    }
+
+    public static async Task<bool> Confirm(this IJSRuntime js, string title, string message, TypeMessageSweetAlert type)
+    {
+        return await js.InvokeAsync<bool>("customConfirm", title, message, type.ToString());
+    }
+
+    public enum TypeMessageSweetAlert
+    {
+        question, warning, error, success, info
+    }
 }
--- Client/wwwroot/js/Utilities.js
+++ Client/wwwroot/js/Utilities.js
@@ -6,3 +6,22 @@ function saveAsFile(fileName, bytesBase64) {
     link.click();
     document.body.removeChild(link);    
 }
+
+function customConfirm(title, message, type) {
+    return new Promise((resolve) => {
+        Swal.fire({
+            title: title,
+            text: message,
+            type: type,
+            showDenyButton: true,
+            confirmButtonText: 'Delete',
+            denyButtonText: 'No, cancel!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resolve(true);
+            } else if (result.isDenied) {
+                resolve(false);
+            }
+        });        
+    })
+}
