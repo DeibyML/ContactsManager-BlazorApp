41e4d462d752e15ec03fad39e4865cfc570095c5
Deiby Montoya <deiby.sk@hotmail.com>
2023-01-01 19:47:21 -0500

    Generic List and RenderFragment with type to show customized list with the same component

--- Client/Helpers/IJSExtensions.cs
+++ Client/Helpers/IJSExtensions.cs
@@ -5,43 +5,43 @@ namespace BlazorCrud.Client.Helpers;
 
 public static class IJSExtensions
 {
-    public static Task SaveAs(this IJSRuntime js, string fileName, byte[] file)
+    public static ValueTask<object> SaveAs(this IJSRuntime js, string fileName, byte[] file)
     {
         return js.InvokeAsync<object>("saveAsFile", 
             fileName,
-            Convert.ToBase64String(file)).AsTask();
+            Convert.ToBase64String(file));
     }
 
-    public static Task ShowAlert(this IJSRuntime js, string message)
+    public static ValueTask<object> ShowAlert(this IJSRuntime js, string message)
     {
-        return js.InvokeAsync<object>("Swal.fire", message).AsTask();
+        return js.InvokeAsync<object>("Swal.fire", message);
     }
     
-    public static Task ShowAlert(this IJSRuntime js, string message, string title, TypeMessageSweetAlert type)
+    public static ValueTask<object> ShowAlert(this IJSRuntime js, string message, string title, TypeMessageSweetAlert type)
     {
-        return js.InvokeAsync<object>("Swal.fire", title, message, type.ToString()).AsTask();
+        return js.InvokeAsync<object>("Swal.fire", title, message, type.ToString());
     }
 
-    public static async Task<bool> Confirm(this IJSRuntime js, string title, string message, TypeMessageSweetAlert type)
+    public static async ValueTask<bool> Confirm(this IJSRuntime js, string title, string message, TypeMessageSweetAlert type)
     {
         return await js.InvokeAsync<bool>("customConfirm", title, message, type.ToString());
     }
     
-    public static Task SetInLocalStorage(this IJSRuntime js, string key, string content) => js.InvokeAsync<object>(
+    public static ValueTask<object> SetInLocalStorage(this IJSRuntime js, string key, string content) => js.InvokeAsync<object>(
             "localStorage.setItem",
             key, content
-        ).AsTask();
+        );
 
-    public static Task<string> GetFromLocalStorage(this IJSRuntime js, string key)
+    public static ValueTask<string> GetFromLocalStorage(this IJSRuntime js, string key)
         => js.InvokeAsync<string>(
             "localStorage.getItem",
             key
-        ).AsTask();
+        );
 
-    public static Task RemoveItem(this IJSRuntime js, string key)
+    public static ValueTask<object> RemoveItem(this IJSRuntime js, string key)
         => js.InvokeAsync<object>(
             "localStorage.removeItem",
-            key).AsTask();
+            key);
 
     public enum TypeMessageSweetAlert
     {
